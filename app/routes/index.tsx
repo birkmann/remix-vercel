7cc81a0d0fb625c5f95ffe23c16e642b08cd0042
Rafael Birkmann <mail@rafaelbirkmann.de>
2022-06-02 12:38:45 +0200

    tailwind

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,7 @@
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="bg-[#1e2c3a] text-white min-h-screen p-8">
+      <h1 className="font-bold text-3xl">Remix</h1>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  mode: "jit",
+  // These paths are just examples, customize them to match your project structure
+  purge: ["./public/**/*.html", "./app/**/*.{js,jsx,ts,tsx,vue}"],
+  theme: {
+    // ...
+  },
+  // ...
+};
